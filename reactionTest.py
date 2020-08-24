569176a7a612c71dbbee321d9d591a59ec032d9c
Jonathan Yee <31420619+TheCountOfPeru@users.noreply.github.com>
2020-08-24 16:59:25 -0600

    Update reactionTest.py

--- reactionTest.py
+++ reactionTest.py
@@ -4,15 +4,10 @@ from os import system, name
 
 nanoSecondsInMilliseconds = 1000000
 
-def playGame():
-    print("Gaming")
-
 def clear(): 
-  
     # for windows 
     if name == 'nt': 
         _ = system('cls') 
-  
     # for mac and linux(here, os.name is 'posix') 
     else: 
         _ = system('clear') 
